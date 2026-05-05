73339fc6c5d87a683fbfe1e1bd7c7357ac4076da
Lakhshamana <lakshamana@protonmail.com>
2026-05-05 14:55:51 -0300

    feat: router functions

--- include/cerv/router.h
+++ include/cerv/router.h
@@ -3,7 +3,7 @@
 
 #include "handler.h"
 
-#define CERV_ROUTER_MAX_ROUTES 64
+#define CERV_ROUTER_MAX_ROUTES 256
 
 typedef struct {
   const char  *method;
@@ -16,7 +16,7 @@ typedef struct {
   int       count;
 } CervRouter;
 
-void         cerv_router_init(CervRouter *r);
+CervRouter*  cerv_router_new();
 void         cerv_router_add(CervRouter *r, const char *method, const char *path, void *handler);
 CervHandler *cerv_router_match(CervRouter *r, const char *method, const char *path);
 
--- src/router.c
+++ src/router.c
@@ -1,21 +1,29 @@
 #include "cerv/router.h"
+#include <stdlib.h>
+#include <string.h>
 
-void cerv_router_init(CervRouter *r) {
-  (void)r;
+CervRouter *cerv_router_new() {
+  CervRouter *r = (CervRouter *)malloc(sizeof(CervRouter));
+  r->count = 0;
+  return r;
 }
 
 void cerv_router_add(CervRouter *r, const char *method, const char *path,
                      void *handler) {
-  (void)r;
-  (void)method;
-  (void)path;
-  (void)handler;
+  int count = r->count;
+  CervRoute route = {.method = method, .path = path, .handler = handler};
+  r->routes[count++] = route;
+  r->count = count;
 }
 
 CervHandler *cerv_router_match(CervRouter *r, const char *method,
                                const char *path) {
-  (void)r;
-  (void)method;
-  (void)path;
-  return nullptr;
+  for (int i = 0; i < r->count; i++) {
+    CervRoute route = r->routes[i];
+    if (strcmp(route.method, method) && strcmp(route.path, path)) {
+      return route.handler;
+    }
+  }
+
+  return NULL;
 }
