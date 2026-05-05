09d181aa3134f94237e80568ccd1ab6956315f6d
Lakhshamana <lakshamana@protonmail.com>
2026-05-05 14:56:29 -0300

    feat: request + response set functions

--- src/response.c
+++ src/response.c
@@ -1,11 +1,14 @@
 #include "cerv/response.h"
+#include <stdlib.h>
+#include <string.h>
 
 void cerv_response_set_body(CervResponse *res, const char *body) {
-  (void)res;
-  (void)body;
+  char *b = malloc(strlen(body) + 1);
+  b[0] = '\0';
+  strcpy(b, body);
+  res->body = b;
 }
 
 void cerv_response_set_status(CervResponse *res, int status) {
-  (void)res;
-  (void)status;
+  res->status = status;
 }
